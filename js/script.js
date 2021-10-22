58521e26591fc07c9c0c643b655301acd8bea65a
rusevaIvelina <iva.ruseva14@gmail.com>
2021-10-22 14:51:59 -0400

    Update script.js
    created the js code for the ToDoList app

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-alert ("ToDoList App Coming");
+//1.Adding anew item to the list:
+    function newItem() {
+
+    let list = $("#list");
+    let li = $("<li></li>");
+    let inputValue = $(input).val();
+    li.append(inputValue);
+  
+    if(inputValue === " ") {
+      alert("You must enter something!");
+    } else {
+      list.append(li);
+    }
+  
+    //2.Crossing an item:
+     function crossOut() {
+  
+       li.toggleClass("strike");
+     }
+
+    //adds eventListener
+     li.on("dblclick", crossOut);
+  
+     //3.Deleting with "X":
+
+       //creating "X" button
+      let crossOutButton = $("<crossOutButton>X</crossOutButton");
+      crossOutButton.append(document.createTextNode("X"));
+      li.append(crossOutButton);
+  
+      //adds eventListener
+      crossOutButton.on("click", deleteListItem);
+  
+      function deleteListItem() {
+        li.addClass("delete");
+      }
+  
+      //4.Reordering the items:
+      $("#list").sortable();
+    }
