278a01050155033ad7a8d2dd4322e5dc984a31e4
rusevaIvelina <iva.ruseva14@gmail.com>
2021-10-23 12:30:41 -0400

    Update script.js
    indenting

--- js/script.js
+++ js/script.js
@@ -1,40 +1,33 @@
-//1.Adding anew item to the list:
-    function newItem() {
+   //1. Adds a new item to thew list:
+   function newItem() {
 
     let list = $("#list");
     let li = $("<li></li>");
-    let inputValue = $("#input").val();
+    let inputValue = $("input").val();
     li.append(inputValue);
-  
-    if(inputValue === " ") {
-      alert("You must enter something!");
+
+    if (inputValue === "") {
+        alert("You must write something!");
     } else {
-      list.append(li);
-    }
-  
-    //2.Crossing an item:
-     function crossOut() {
-  
-       li.toggleClass("strike");
-     }
-
-    //adds eventListener
-     li.on("dblclick", crossOut);
-  
-     //3.Deleting with "X":
-
-       //creating "X" button
-      let crossOutButton = $("<crossOutButton>X</crossOutButton");
-      crossOutButton.append(document.createTextNode("X"));
-      li.append(crossOutButton);
-  
-      //adds eventListener
-      crossOutButton.on("click", deleteListItem);
-  
-      function deleteListItem() {
-        li.addClass("delete");
+        list.append(li);
       }
-  
-      //4.Reordering the items:
+    //2. Crosses an item from the list:
+
+    function crossOut() {
+        li.toggleClass("strike");
+      }
+
+      li.on("dblclick", crossOut);
+
+    //3. Deletes an item with "X";
+    let crossOutButton = $("<crossOutButton>X</crossOutButton>");
+    li.append(crossOutButton);
+    crossOutButton.on("click", deleteListItem);
+
+    function deleteListItem() {
+      li.addClass("delete");
+      }
+
+    // 4. Reordering the items:
       $("#list").sortable();
-    }
+  }
